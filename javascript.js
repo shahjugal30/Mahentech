8478de23eb2418e927e66d4ba80c962654f89db1
shahjugal30 <57308723+shahjugal30@users.noreply.github.com>
2020-06-01 13:11:09 +0530

    navbar with animation done

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function mouseOverceh() {
+    document.getElementById("cehimg").style.display = "block";
+    document.getElementById("basicimg").style.display = "none";
+
+}
+
+function mouseOutceh() {
+    document.getElementById("cehimg").style.display = "none";
+    document.getElementById("basicimg").style.display = "block";
+}
+
+function mouseOvercpe() {
+    document.getElementById("cpeimg").style.display = "block";
+    document.getElementById("basicimg").style.display = "none";
+
+}
+
+function mouseOutcpe() {
+    document.getElementById("cpeimg").style.display = "none";
+    document.getElementById("basicimg").style.display = "block";
+}
+
+function mouseOverdropdown() {
+    document.getElementById("dropdown-content").style.display = "block";
+
+}
+
+function mouseOutdropdown() {
+    document.getElementById("dropdown-content").style.display = "none";
+}
