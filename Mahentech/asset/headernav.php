6f40994e0ccb2bb4f8d728e2aa14d71ce1326767
Samkit-shah <65543606+Samkit-shah@users.noreply.github.com>
2020-06-05 10:29:54 +0530

    Project in php Date:05-06-2020
    all files in php

---
+++ Mahentech/asset/headernav.php
@@ -0,0 +1,93 @@
+ <!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MahenTech</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src='javascript/javascript.js'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" media=" screen and (max-width: 1110px)" href="css/mobilestyle.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="jquery-3.5.1.min.js"></script>
+</head>
+     <header class="con">
+            <div class="container">
+                <div class="menu-toggle" style="margin-bottom: 15px;">
+                    <span>&#9776;</span>
+                </div>
+                <div class="logo_navbar_left">
+                    <a href="#" title="mahentech">
+                        <img class="logo" src="images/mahentech.png" alt="mahentech">
+                    </a>
+                </div>
+
+
+                <nav class="standard navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="menu-item-0">
+                            <a  id="home" href="index.php">Home</a>
+                        </li>
+                        <li class="menu-item-1">
+                            <a href="#">About Us</a>
+                        </li>
+
+                        <li class="menu-item-2">
+                            <a href="#">Services</a>
+                        </li>
+                        <li class="menu-item-3">
+                            <a href="#">Events</a>
+                        </li>
+                        <li class="menu-item-4">
+                            <div class="dropdown">
+                                <button class="dropbtn">Training 
+                                <i class="fa fa-caret-down"></i>
+                         </button>
+
+                                <div class="dropdown-content" id="dropdown-content">
+
+                                    <div class="grid-container">
+                                        <div id="image" >
+                                            <img class="basicimg" id="basicimg" src="images/q1.jpg" width="450" height="500">
+                                            <img class="cehimg" id="cehimg" src="images/q2.jpg" width="450" height="500" style = " display:none" >
+                                            <img class="cpeimg" id="cpeimg" src="images/q3.jpg" width="450" height="500" style = " display:none">
+                                        </div>
+
+                                        <div class="courses">
+                                            <br><br>
+                                            <div class="ceh" onmouseover="mouseOverceh()" onmouseout="mouseOutceh()"><i class='fas fa-long-arrow-alt-right' style='font-size:25px'></i><a style = " text-decoration: none; color: black; font-size: 25px;">Certified Ethical Hacker</a></div>
+                                           
+                                            <div class="cpe" onmouseover="mouseOvercpe()" onmouseout="mouseOutcpe()"><i class='fas fa-long-arrow-alt-right' style='font-size:25px'></i><a style = " text-decoration: none; color: black; font-size: 25px;">cpe</a></div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item-5-mobile">
+
+                            <a href="#">Training</a>
+
+                        </li>
+
+                        <li class="menu-item-6">
+                            <a href="#">Security Tips</a>
+                        </li>
+                        <li class="menu-item-7">
+                            <a  id="contactus" href="contactus.php">Contact Us</a>
+                        </li>
+
+
+                    </ul>
+                </nav>
+
+            </div>
+
+
+
+        </header>
---
+++ Mahentech/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+   <style>
+       
+        #home {
+            text-decoration: none;
+            background-color: inherit;
+            margin-bottom: 12px;
+          
+            }
+    </style>
+</head>
+<body>
+
+    <div class="content-holder">
+       <?php include('asset/headernav.php') ?>
+       <div class = "top_background">
+           
+        </div>
+    </div>
+</body>
+
+
+
+</html>
