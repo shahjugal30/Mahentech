d9da3cdd948f8a4a243534137c918564faf30708
shahjugal30 <57308723+shahjugal30@users.noreply.github.com>
2020-06-06 08:57:13 +0530

    Add files via upload

---
+++ Mahentech/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    
+   <link rel="stylesheet" type="text/css" href="css/indexcss.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+<link rel="stylesheet" href="https://unpkg.com/flickity-fade@1/flickity-fade.css">        
+   
+</head>
+<style media = "screen">
+	
+	.carousel-cell{	
+	width: 68%;
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	}
+	
+	.carousel.is-fullscreen .carousel-cell {
+	height: 100%;
+	}
+	.w3-image {
+	display: block;
+	max-height: 100%;
+	}
+	
+}
+  </style>
+
+<body>
+
+    <div class="content-holder">
+        <?php include('asset/headernav.php') ?>
+        <div class="top_background">
+            <div class="introduction">
+                <h1 class="intro">CERTIFIED ETHICAL HACKING PRACTITIONER</h1>
+                <h3 class="intro2">Sachin Dedhia is an independent Cyber Crime Investigator & also a Certified Ethical Hacker (EC-Council,USA).</h3>
+                <button class="knowmore_btn">Know More</button>
+            </div>
+        </div>
+		
+		<div class = "carousel" data-flickity ='{"fade": true, "imagesLoaded": true,"autoPlay": 2500, "pauseAutoPlayOnHover": false}'>
+		<div class = "carousel-cell">
+			<img class = "w3-image" src = "images/coding1.jpg">
+		</div>
+		<div class = "carousel-cell">
+			<img class = "w3-image" src = "images/coding2.jpg">
+		</div>
+		<div class = "carousel-cell">
+			<img class = "w3-image" src = "images/coding3.jpg">
+		</div>
+	</div>
+         
+   
+
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>wrbvwgy5nb  <br><br><br>
+    </div>
+	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+	<script src="https://unpkg.com/flickity-fade@1/flickity-fade.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+</body>
+</html>
