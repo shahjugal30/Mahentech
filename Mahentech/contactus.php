04983ea43177820490b085faedb9906bfbd35264
Samkit-shah <65543606+Samkit-shah@users.noreply.github.com>
2020-06-20 09:01:29 +0530

    Added ABoutUS and changes in contact us.

--- Mahentech/contactus.php
+++ Mahentech/contactus.php
@@ -7,113 +7,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         <style type="text/css" media="screen">
-            #contactus_nav {
-                text-decoration: none;
-                background-color: inherit;
-                transform: scale(1.3);
-                text-shadow: 1px 1px white;
-            }
-
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                font-family: helvetica;
-            }
-
-            body {
-                background-color: inherit;
-            }
-
-            .main {
-                width: 500px;
-                margin: 50px auto;
-                border-radius: 10px;
-                border: 5px solid #dc143c;
-                border-left: 40px solid #dc143c;
-                box-shadow: 1px 2px 10px #555;
-            }
-
-            .info {
-                width: 100%;
-                background-color: #dc143c;
-                padding: 7px;
-                text-shadow: 1px 1px 1px #222;
-                color: #fff;
-                font-size: 20px;
-            }
-
-            .form-box {
-                padding: 20px;
-                background-color: #eee;
-            }
-
-            label {
-                color: navy;
-                font-size: 18px;
-            }
-
-            .inp,
-            .msg-box {
-                width: 100%;
-                padding: 10px;
-                margin-top: 4px;
-                margin-bottom: 5px;
-                border-radius: 5px;
-                border: 2px solid #dc143c;
-                font-weight: bold;
-                color: #dc143c;
-                border-right: 15px solid #dc143c;
-                border-left: 15px solid #dc143c;
-                resize: none;
-            }
-
-            .msg-box {
-                height: 80px;
-            }
-
-            .inp:focus,
-            .msg-box:focus {
-                outline: none;
-                border: 2px solid navy;
-                border-right: 15px solid navy;
-                border-left: 15px solid navy;
-            }
-
-            .sub-btn {
-                width: 100%;
-                padding: 10px;
-                border-radius: 5px;
-                margin-top: 5px;
-                border: none;
-                background: linear-gradient(#dc143c, #800000);
-                cursor: pointer;
-                color: #fff;
-                font-size: 20px;
-                text-shadow: 1px 1px 1px #444;
-            }
-
-            .sub-btn:hover {
-                background: linear-gradient(#800000, #dc143c);
-                opacity: 0.8;
-                transition: all ease-out 0.2s;
-            }
-
-            .sub-btn:focus {
-                outline: none;
-            }
-
-            <blade media|(max-width%3A%20720px)%20%7B%0D>.main {
-                width: 90%;
-            }
-            }
 
         </style>
+        <link rel="stylesheet" type="text/css" href="css/contactus.css">
     </head>
 
     <body>
-        <div class="main">
-            <div class="info">Give Your Feedback!</div>
+        <div class="main_contactus">
+            <div class="info">Fill Your Details, We will Contact You</div>
             <form action="mail_handler.php" method="post" name="form" class="form-box">
                 <label for="name">Name</label><br>
                 <input type="text" name="name" class="inp" placeholder="Enter Your Name" required><br>
--- Mahentech/course_contactform.php
+++ Mahentech/course_contactform.php
@@ -4,104 +4,34 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <style type="text/css" media="screen">
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                font-family: helvetica;
-            }
-
-            body {
-                background-color: inherit;
-            }
-
-            .main {
-                width: autox;
-                margin: 50px auto;
-                border-radius: 10px;
-                border: 5px solid black;
-                box-shadow: 1px 2px 10px #555;
-            }
-
-            .info {
+        <link rel="stylesheet" type="text/css" href="css/contactus.css">
+        <style>
+            .main_contactus {
                 width: auto;
-                background-color: black;
-                padding: 7px;
-                text-shadow: 1px 1px 1px #222;
-                color: #fff;
-                font-size: 20px;
             }
 
-            .form-box {
-                padding: 20px;
-                background-color: #eee;
-            }
-
-            label {
-                color: black;
-                font-size: 18px;
-            }
-
-            .inp,
-            .msg-box {
-                width: 100%;
-                padding: 10px;
-                margin-bottom: 5px;
-                font-weight: bold;
-                color: #dc143c;
-
-            }
-
-
-
-            .inp:focus,
-            .msg-box:focus {
-                outline: none;
-                border: 3px solid black;
-            }
-
-            .sub-btn {
-                width: auto;
-                padding: 10px;
-                border-radius: 5px;
-                margin-top: 5px;
-                border: none;
-                background: linear-gradient(#494143, #0e0d0d);
-                cursor: pointer;
-                color: #fff;
-                font-size: 20px;
-                text-shadow: 1px 1px 1px rgb(170, 168, 168);
-            }
-
-            .sub-btn:hover {
-                background: linear-gradient(#494848, #0f0e0f);
-                opacity: 0.8;
-                transition: all ease-out 0.2s;
-            }
-
-            .sub-btn:focus {
-                outline: none;
-            }
-
-            <blade media|(max-width%3auto720px)%20%7B%0D>.main {
-                width: auto
-            }
-            }
-
-            #contactus {
+            #contactus_nav {
                 text-decoration: none;
                 background-color: inherit;
-                margin-bottom: 12px;
+                transform: none;
+                text-shadow: none;
+            }
+
+            #contactus_nav:hover {
+                text-decoration: none;
+                background-color: inherit;
+                transition-duration: 0.3s;
+                transform: scale(1.3);
+                text-shadow: 1px 1px white;
+                cursor: pointer;
             }
 
         </style>
-    </head>
 
+    </head>
     <body>
-        <div class="main">
-            <div class="info">Contact Us</div>
+        <div class="main_contactus">
+            <div class="info">Fill Your Details, We will Contact You</div>
             <form action="mail_handler.php" method="post" name="form" class="form-box">
                 <label for="name">Name</label><br>
                 <input type="text" name="name" class="inp" placeholder="Enter Your Name" required><br>
@@ -110,10 +40,10 @@
                 <label for="phone">Phone</label><br>
                 <input type="tel" name="phone" class="inp" placeholder="Enter Your Phone" required><br>
 
-                <br>
+
                 <input type="submit" name="submit" value="Send" class="sub-btn">
             </form>
         </div>
-    </body>
 
+    </body>
 </html>
